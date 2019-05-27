1d59a14f1201ac55fa0a6e16e4032a07ca421790
Aditya Diwakar <aditya@diwakar.io>
2019-05-26 22:17:02 -0400

    Added getKey() function

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package sendpulse
 
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"io/ioutil"
+	"bytes"
+)
+
 var grantType string
 var clientID string
 var clientSecret string
@@ -10,3 +18,31 @@ func initialize(rClientID string, rClientSecret string) {
 	clientSecret = rClientSecret
 }
 
+func getKey() {
+	requestBody, err := json.Marshal(map[string]string{
+		"grant_type": grantType,
+		"client_id": clientID,
+		"client_secret": clientSecret,
+	})
+	
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	resp, err := http.Post("https://api.sendpulse.com/oauth/access_token", 
+						   "application/json",
+						   bytes.NewBuffer(requestBody))
+
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	log.Println(string(body))
+}
