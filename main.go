1b2070e21bf2e9a9daa203a7ad50c992715429de
Aditya Diwakar <aditya@diwakar.io>
2019-05-26 23:19:50 -0400

    Add JSON struct

--- main.go
+++ main.go
@@ -2,15 +2,22 @@ package sendpulse
 
 import (
 	"encoding/json"
-	"log"
 	"net/http"
 	"io/ioutil"
 	"bytes"
+	"errors"
 )
 
 var grantType string
 var clientID string
 var clientSecret string
+var accessToken string
+
+
+type oauthTokenResponse struct {
+	AccessToken string `json:"access_token"`
+	ErrorCode   int	   `json:"error_code"`
+}
 
 func initialize(rClientID string, rClientSecret string) {
 	grantType = "client_credentials"
@@ -18,7 +25,7 @@ func initialize(rClientID string, rClientSecret string) {
 	clientSecret = rClientSecret
 }
 
-func getKey() {
+func getKey() (string, error) {
 	requestBody, err := json.Marshal(map[string]string{
 		"grant_type": grantType,
 		"client_id": clientID,
@@ -26,7 +33,7 @@ func getKey() {
 	})
 	
 	if err != nil {
-		log.Fatalln(err)
+		return "", errors.New("Marshalling request payload gave an error.")
 	}
 
 	resp, err := http.Post("https://api.sendpulse.com/oauth/access_token", 
@@ -34,15 +41,24 @@ func getKey() {
 						   bytes.NewBuffer(requestBody))
 
 	if err != nil {
-		log.Fatalln(err)
+		return "", errors.New("Making the request gave an error.")
 	}
 
 	defer resp.Body.Close()
 
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		log.Fatalln(err)
+		return "", errors.New("Reading the response gave an error.")
 	}
 
-	log.Println(string(body))
+	var response oauthTokenResponse
+	err = json.Unmarshal([]byte(body), &response)
+
+	if response.ErrorCode != 0 {
+		return "", errors.New("SendPulse sent an error.")
+	}
+
+	accessToken = response.AccessToken
+	return response.AccessToken, nil
 }
+
